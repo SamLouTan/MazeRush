abdf69d5035a23f47da5a9fc6065c0650170dbe1
Arthur Pineau <arthupineau04@gmail.com>
2023-10-18 09:38:37 +0200

    Charactere asset
    Charactere Move + Jump + Crouch
    With weapon

--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -1,73 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
-
-public class InputManager : MonoBehaviour
+namespace MoveCharactere.Manager
 {
-    [SerializeField] private PlayerInput PlayerInput;
-
-    // read-only Variable pour savoir le status des inputs
-    public Vector2 Move { get; private set; }
-    public Vector2 Look { get; private set; }
-    public bool Run { get; private set; }
-
-    //Acces a la Current InputActionMap et aux InputAction Individuellement
-    private InputActionMap _currentMap;
-    private InputAction _moveAction;
-    private InputAction _lookAction;
-    private InputAction _runAction;
-
-    //Obtention de la InputMap et attache au inputAction
-    private void Awake()
+    public class InputManager : MonoBehaviour
     {
-        Hidecursor();
-        _currentMap = PlayerInput.currentActionMap;
-        _moveAction = _currentMap.FindAction("Move");
-        _lookAction = _currentMap.FindAction("Look");
-        _runAction = _currentMap.FindAction("Run");
+        [SerializeField] private PlayerInput PlayerInput; 
+        
+        // read-only Variable pour savoir le status des inputs
+        public Vector2 Move { get; private set; }
+        public Vector2 Look { get; private set; }
+        public bool Run { get; private set; }
+        public bool Jump { get; private set; }
+        public bool Crouch { get; private set; }
+        
+        
+        //Acces a la Current InputActionMap et aux InputAction Individuellement
+        private InputActionMap _currentMap;
+        private InputAction _moveAction;
+        private InputAction _lookAction;
+        private InputAction _runAction;
+        private InputAction _jumpAction;
+        private InputAction _crouchAction;
+        
+        //Obtention de la InputMap et attache au inputAction
+        private void Awake()
+        {
+            Hidecursor();
+            _currentMap = PlayerInput.currentActionMap;
+            _moveAction = _currentMap.FindAction("Move");
+            _lookAction = _currentMap.FindAction("Look");
+            _runAction = _currentMap.FindAction("Run");
+            _jumpAction = _currentMap.FindAction("Jump");
+            _crouchAction = _currentMap.FindAction("Crouch");
+            
+            //Start Fonction
+            _moveAction.performed += OnMove;
+            _lookAction.performed += OnLook;
+            _runAction.performed += OnRun;
+            _jumpAction.performed += OnJump;
+            _crouchAction.performed += OnCrouch;
+            
+            //Stop Fonction
+            _moveAction.canceled += OnMove;
+            _lookAction.canceled += OnLook;
+            _runAction.canceled += OnRun;
+            _jumpAction.canceled += OnJump;
+            _crouchAction.canceled += OnCrouch;
+        }
 
-        //Start Fonction
-        _moveAction.performed += OnMove;
-        _lookAction.performed += OnLook;
-        _runAction.performed += OnRun;
+        private void Hidecursor()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
 
-        //Stop Fonction
-        _moveAction.canceled += OnMove;
-        _lookAction.canceled += OnLook;
-        _runAction.canceled += OnRun;
-    }
+        //Onmove Method
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            Move = context.ReadValue<Vector2>();
+        }
+        
+        //OnLook Method
+        private void OnLook(InputAction.CallbackContext context)
+        {
+            Look = context.ReadValue<Vector2>();
+        }
+        
+        //OnRun Method
+        private void OnRun(InputAction.CallbackContext context)
+        {
+            Run = context.ReadValueAsButton();
+        }
+        
+        private void OnJump(InputAction.CallbackContext context)
+        { 
+            Jump = context.ReadValueAsButton();
+        }
+        
+        private void OnCrouch(InputAction.CallbackContext context)
+        { 
+            Crouch = context.ReadValueAsButton();
+        }
+        private void OnEnable()
+        {
+            _currentMap.Enable();
+        }
 
-    private void Hidecursor()
-    {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    //Onmove Method
-    private void OnMove(InputAction.CallbackContext context)
-    {
-        Move = context.ReadValue<Vector2>();
-    }
-
-    //OnLook Method
-    private void OnLook(InputAction.CallbackContext context)
-    {
-        Look = context.ReadValue<Vector2>();
-    }
-
-    //OnRun Method
-    private void OnRun(InputAction.CallbackContext context)
-    {
-        Run = context.ReadValueAsButton();
-    }
-
-    private void OnEnable()
-    {
-        _currentMap.Enable();
-    }
-
-    private void OnDisable()
-    {
-        _currentMap.Disable();
+        private void OnDisable()
+        {
+            _currentMap.Disable();
+        }
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,89 +1,162 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MoveCharactere.Manager;
+using Unity.VisualScripting;
 using UnityEngine;
 
-
-public class PlayerController : MonoBehaviour
+namespace MoveCharactere.PlayerControl
 {
-    [SerializeField] private float AnimBlendSpeed = 8.9f;
-
-    [SerializeField] private Transform CameraRoot;
-    [SerializeField] private Transform Camera;
-    [SerializeField] private float UpperLimit = -40f;
-    [SerializeField] private float Bottomlimit = 70f;
-    [SerializeField] private float Sensitivity = 21.9f;
-    private Rigidbody _playerRigidbody;
-    private InputManager _inputManager;
-
-    //Acces aux Animations
-    private Animator _animator;
-    private bool _hasAnimator;
-
-    //Acces aux HashID de Xvelocity et Yvelocity
-    private int _xValHash;
-    private int _yValHash;
-
-    //Camera Variables
-    private float _xRotation;
-
-    //Player vitesse
-    private const float _walkSpeed = 2f;
-    private const float _runSpeed = 6f;
-
-    private Vector2 _currentVelocity;
-
-    private void Awake()
+    public class PlayerController : MonoBehaviour
     {
-        _hasAnimator = TryGetComponent<Animator>(out _animator);
-        _playerRigidbody = GetComponent<Rigidbody>();
-        _inputManager = GetComponent<InputManager>();
+        [SerializeField] private float AnimBlendSpeed = 8.9f;
 
-        _xValHash = Animator.StringToHash("X_Velocity");
-        _yValHash = Animator.StringToHash("Y_Velocity");
-    }
+        [SerializeField] private Transform CameraRoot;
+        [SerializeField] private Transform Camera;
+        [SerializeField] private float UpperLimit = -40f;
+        [SerializeField] private float Bottomlimit = 70f;
+        [SerializeField] private float Sensitivity = 21.9f;
+        [SerializeField] private float JumpFactor = 260f;
+        [SerializeField] private float Dis2Ground = 0.8f;
+        [SerializeField] private LayerMask GroundCheck;
+        [SerializeField] private float AirResistance = 0.8f;
+        private Rigidbody _playerRigidbody;
+        private InputManager _inputManager;
+        
+        //Acces aux Animations
+        private Animator _animator;
+        private bool _grounded = false;
+        private bool _hasAnimator;
+        
+        //Acces aux HashID de Xvelocity et Yvelocity
+        private int _xValHash;
+        private int _yValHash;
+        private int _zValHash;
+        private int _jumpHash;
+        private int _groundedHash;
+        private int _fallingHash;
+        private int _crouchHash;
+        
+        //Camera Variables
+        private float _xRotation;
+        
+        
+        //Player vitesse
+        private const float _walkSpeed = 2f;
+        private const float _runSpeed = 6f;
 
-    private void FixedUpdate()
-    {
-        Move();
-    }
+        private Vector2 _currentVelocity;
+        private void Start()
+        {
+            _hasAnimator = TryGetComponent<Animator>(out _animator);
+            _playerRigidbody = GetComponent<Rigidbody>();
+            _inputManager = GetComponent<InputManager>();
 
-    private void LateUpdate()
-    {
-        CamMouvements();
-    }
+            _xValHash = Animator.StringToHash("X_Velocity");
+            _yValHash = Animator.StringToHash("Y_Velocity");
+            _zValHash = Animator.StringToHash("Z_Velocity");
+            _jumpHash = Animator.StringToHash("Jump");
+            _fallingHash = Animator.StringToHash("Falling");
+            _groundedHash = Animator.StringToHash("Grounded");
+            _crouchHash = Animator.StringToHash("Crouch");
 
-    private void Move()
-    {
-        if (!_hasAnimator) return;
+        }
 
-        float targetSpeed = _inputManager.Run ? _runSpeed : _walkSpeed;
-        if (_inputManager.Move == Vector2.zero) targetSpeed = 0.1f;
+        private void FixedUpdate()
+        {
+            SampleGround();
+            Move();
+            HandleJump();
+            HandleCrouch();
+        }
 
-        _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, _inputManager.Move.x * targetSpeed,
-            AnimBlendSpeed * Time.deltaTime);
-        _currentVelocity.y = Mathf.Lerp(_currentVelocity.y, _inputManager.Move.y * targetSpeed,
-            AnimBlendSpeed * Time.deltaTime);
+        private void LateUpdate()
+        {
+            CamMouvements();
+        }
 
-        var xVelDifference = _currentVelocity.x - _playerRigidbody.velocity.x;
-        var zVelDifference = _currentVelocity.y - _playerRigidbody.velocity.y;
+        private void Move()
+        {
+            if (!_hasAnimator) return;
 
-        _playerRigidbody.AddForce(transform.TransformVector(new Vector3(xVelDifference, 0, zVelDifference)),
-            ForceMode.VelocityChange);
+            float targetSpeed = _inputManager.Run ? _runSpeed : _walkSpeed;
+            if(_inputManager.Crouch) targetSpeed = 1.5f;
+            if (_inputManager.Move == Vector2.zero) targetSpeed = 0;
+            
+            //A Supprimer si on veut deplacement dans l'air
+            if (_grounded)
+            {
+                _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, _inputManager.Move.x * targetSpeed, AnimBlendSpeed * Time.deltaTime);
+                _currentVelocity.y = Mathf.Lerp(_currentVelocity.y, _inputManager.Move.y * targetSpeed, AnimBlendSpeed * Time.deltaTime);
 
-        _animator.SetFloat(_xValHash, _currentVelocity.x);
-        _animator.SetFloat(_yValHash, _currentVelocity.y);
-    }
+                var xVelDifference = _currentVelocity.x - _playerRigidbody.velocity.x;
+                var zVelDifference = _currentVelocity.y - _playerRigidbody.velocity.y;
+                
+                _playerRigidbody.AddForce(transform.TransformVector(new Vector3(xVelDifference,0,zVelDifference)),ForceMode.VelocityChange);   
+            }
+            else
+            {
+                _playerRigidbody.AddForce(transform.TransformVector(new Vector3(_currentVelocity.x * AirResistance ,0, _currentVelocity.y * AirResistance)), ForceMode.VelocityChange);
+            }
 
-    private void CamMouvements()
-    {
-        if (!_hasAnimator) return;
+            _animator.SetFloat(_xValHash,_currentVelocity.x);
+            _animator.SetFloat(_yValHash,_currentVelocity.y);
+        }
 
-        var Mouse_X = _inputManager.Look.x;
-        var Mouse_Y = _inputManager.Look.y;
-        Camera.position = CameraRoot.position;
+        private void CamMouvements()
+        {
+            if (!_hasAnimator) return;
 
-        _xRotation -= Mouse_Y * Sensitivity * Time.deltaTime;
-        _xRotation = Mathf.Clamp(_xRotation, UpperLimit, Bottomlimit);
+            var Mouse_X = _inputManager.Look.x;
+            var Mouse_Y = _inputManager.Look.y;
+            Camera.position = CameraRoot.position;
 
-        Camera.localRotation = Quaternion.Euler(_xRotation, 0, 0);
-        _playerRigidbody.MoveRotation(_playerRigidbody.rotation * Quaternion.Euler(0, Mouse_X * Sensitivity * Time.smoothDeltaTime, 0));
+            _xRotation -= Mouse_Y * Sensitivity * Time.smoothDeltaTime;
+            _xRotation = Mathf.Clamp(_xRotation, UpperLimit, Bottomlimit);
+            
+            Camera.localRotation = Quaternion.Euler(_xRotation,0, 0);
+            _playerRigidbody.MoveRotation(_playerRigidbody.rotation * Quaternion.Euler(0, Mouse_X * Sensitivity * Time.smoothDeltaTime, 0));
+        }
+
+        private void HandleCrouch() => _animator.SetBool(_crouchHash, _inputManager.Crouch);
+        private void HandleJump()
+        {
+            if(!_hasAnimator) return;
+            if(!_inputManager.Jump) return;
+            _animator.SetTrigger(_jumpHash);
+        }
+
+        public void JumpAddForce()
+        { 
+            _playerRigidbody.AddForce(-_playerRigidbody.velocity.y * Vector3.up , ForceMode.VelocityChange);
+            _playerRigidbody.AddForce(Vector3.up * JumpFactor, ForceMode.Impulse);  
+            _animator.ResetTrigger(_jumpHash);
+        }
+
+        private void SampleGround()
+        {
+            if (!_hasAnimator) return;
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(_playerRigidbody.worldCenterOfMass, Vector3.down, out hitInfo, Dis2Ground + 0.1f,
+                    GroundCheck))
+            {
+                //Collided with sth
+                //Grounded
+                _grounded = true;
+                SetAnimationGrounding();
+                return;
+            }
+            _grounded = false;
+            _animator.SetFloat(_zValHash, _playerRigidbody.velocity.y);
+            SetAnimationGrounding();
+            //Falling
+        }
+
+        private void SetAnimationGrounding()
+        {
+            _animator.SetBool(_fallingHash, !_grounded);
+            _animator.SetBool(_groundedHash, _grounded);
+        }
     }
 }
